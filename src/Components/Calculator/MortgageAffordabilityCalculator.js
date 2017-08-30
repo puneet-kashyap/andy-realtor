bd42e9b31d470616772c16513f9a580f8c64e662
Puneet Kashyap <pkashyaps@gmail.com>
2017-08-30 16:50:02 -0400

    Develop (#1)
    * firebase added
    
    * initial commit for calculator module

---
+++ src/Components/Calculator/MortgageAffordabilityCalculator.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from '../navbar';
+import Footer from '../footer';
+
+const MortgageAffordabilityCalculator = () => {
+    return (
+        <div>
+        <Navbar />
+        <section className="success" id="about">
+          <div className="container">
+            <div className="row">
+                <div className="col-lg-12 text-center" style={{'margin':'25px 0px'}}>
+                    <h1>Mortgage Affordability Calculator</h1>
+                    <hr className="star-primary"></hr>
+                </div>
+            </div>
+            <div className="row" style={{'margin':'20px 0px'}}>
+                <div className="widget" data-widget="calc-affordability" data-province="ON" data-lang="en"></div>
+            </div>
+          </div>
+        </section>
+        <Footer />
+        </div>
+    );
+}
+export default MortgageAffordabilityCalculator;
--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ import TermInsurance from './Components/Insurance/TermInsurance';
 import UniversalLifeInsurance from './Components/Insurance/UniversalLifeInsurance';
 import CriticalIllnessInsurance from './Components/Insurance/CriticalIllnessInsurance';
 import WholeLifeInsurance from './Components/Insurance/WholeLifeInsurance';
+import MortgageAffordabilityCalculator from './Components/Calculator/MortgageAffordabilityCalculator';
+import CMHCinsuranceCalculator from './Components/Calculator/CMHCinsuranceCalculator';
+import LandTransferTaxCalculator from './Components/Calculator/LandTransferTaxCalculator';
+import MortgageCalculator from './Components/Calculator/MortgageCalculator';
+import MortgageComparison from './Components/Calculator/MortgageComparison';
 
 import registerServiceWorker from './registerServiceWorker';
 
@@ -21,6 +26,11 @@ ReactDOM.render((
       <Route exact path='/universal-life-insurance' component={UniversalLifeInsurance}/>
       <Route exact path='/critical-illness-insurance' component={CriticalIllnessInsurance}/>
       <Route exact path='/whole-life-insurance' component={WholeLifeInsurance}/>
+      <Route exact path='/Mortgage-Affordability-Calculator' component={MortgageAffordabilityCalculator}/>
+      <Route exact path='/CMHC-Insurance-Calculator' component={CMHCinsuranceCalculator}/>
+      <Route exact path='/Land-Transfer-Tax-Calculator' component={LandTransferTaxCalculator}/>
+      <Route exact path='/Mortgage-Calculator' component={MortgageCalculator}/>
+      <Route exact path='/Mortgage-Comparison' component={MortgageComparison}/>
       <Route exact path='/Contact' component={Contact}/>
       <Route path="*" render={() => (<Redirect to="/"/>)} />
     </Switch>
