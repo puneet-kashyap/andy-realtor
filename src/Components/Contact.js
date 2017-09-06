af66fa608dd53cee8116ede9fffe78fdeac627dc
Puneet Kashyap <pkashyaps@gmail.com>
2017-09-06 10:17:28 -0400

    Develop (#4)
    * firebase added
    
    * initial commit for calculator module
    
    * initial commit for mortgage hub
    
    * initial commit for kw homes and biz for sale tab
    
    * initial commit for facebook social plugin

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -40,13 +40,23 @@ const Contact = () => {
                         </div>
                 </div>
             </div>
-                <div className="row">
+              <div className="row">
                 <div className="col-lg-12 text-center">
                     <h2>Let&apos;s Chat.</h2>
                     <hr className="star-primary"></hr>
                     <iframe title='chat-bot' width="100%" height="500" src="https://console.api.ai/api-client/demo/embedded/3b9af493-8128-477e-b731-676baddb3ffb"></iframe>
                 </div>
-            </div>
+              </div>
+              <div className="row text-center" style={{'margin':'50px 10px 10px 10px'}}>
+                    <div className="fb-page visible-xs"
+                      data-href="https://www.facebook.com/TeamAndyNagpal"
+                      data-tabs="timeline,messages,events"
+                      data-show-facepile="true">
+                      <blockquote cite="https://www.facebook.com/TeamAndyNagpal" className="fb-xfbml-parse-ignore">
+                      <a href="https://www.facebook.com/TeamAndyNagpal">Andy Nagpal Realty Team - Your Home Sold Guaranteed or I&#039;ll buy it.</a>
+                      </blockquote>
+                    </div>
+              </div>
         </div>
     </section>
     <Footer />
